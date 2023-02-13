e617a388bae297e6ae7bf033c6f7823913f45c8b
CLX\lx <liuxiang20XX@163.com>
2023-02-13 18:17:53 +0800

    自动登录初版

--- branches/V2.0.0/cloudfunctions/getList/index.js
+++ branches/V2.0.0/cloudfunctions/getList/index.js
@@ -2,11 +2,18 @@
 const cloud = require('wx-server-sdk')
 
 cloud.init({ // 初始化云开发环境
-  env: cloud.DYNAMIC_CURRENT_ENV // 当前环境的常量
+    env: cloud.DYNAMIC_CURRENT_ENV // 当前环境的常量
 })
 const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (context) => {
-  return await db.collection(context.list).get()
+    let openid = cloud.getWXContext().OPENID
+    return await db.collection(context.list).where(_.or([{
+            _openid: openid
+        },
+        {
+            _openidB: openid
+        }
+    ])).get()
 }
